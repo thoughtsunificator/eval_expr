b681ccc779eeff7d4f1f4bea93f91ee89cbdc948
thoughtsunificator <85041649+thoughtsunificator@users.noreply.github.com>
2021-09-19 10:22:03 +0200

    mocha

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,89 +1,76 @@
+import assert from "assert"
 import { Calculator } from '../index.js'
 
-export function addition(test) {
-	test.expect(6)
-	test.strictEqual(Calculator.compute( "5 + 1" ), 6)
-	test.strictEqual(Calculator.compute( "1 + 1 + 1" ), 3)
-	test.strictEqual(Calculator.compute( "100 + 25 + 0 + 0 + 9" ), 134)
-	test.strictEqual(Calculator.compute( "51 + 111 + 5 + 1000 + 2" ), 1169)
-	test.strictEqual(Calculator.compute( "234 + 324 + 23543.5 + 1.5" ), 24103)
-	test.strictEqual(Calculator.compute( "38 +- 14 + 91 + 1 + 3 +- 2" ), 117)
-	test.done()
-}
+describe("calculator", () => {
 
-export function substraction(test) {
-	test.expect(5)
-	test.strictEqual(Calculator.compute( "0 - 1" ), -1)
-	test.strictEqual(Calculator.compute( "1 - 1 - 13243" ), -13243)
-	test.strictEqual(Calculator.compute( "51 - 111 - 5 - 1000 - 2" ), -1067)
-	test.strictEqual(Calculator.compute( "234 - 324 - 23543.5 - 1.5" ), -23635)
-	test.strictEqual(Calculator.compute( "-25 - 25 - 0 - -120 - 9" ), 61)
-	test.done()
-}
+	it("addition", () => {
+		assert.strictEqual(Calculator.compute( "5 + 1" ), 6)
+		assert.strictEqual(Calculator.compute( "1 + 1 + 1" ), 3)
+		assert.strictEqual(Calculator.compute( "100 + 25 + 0 + 0 + 9" ), 134)
+		assert.strictEqual(Calculator.compute( "51 + 111 + 5 + 1000 + 2" ), 1169)
+		assert.strictEqual(Calculator.compute( "234 + 324 + 23543.5 + 1.5" ), 24103)
+		assert.strictEqual(Calculator.compute( "38 +- 14 + 91 + 1 + 3 +- 2" ), 117)
+	})
 
-export function division(test) {
-	test.expect(4)
-	test.strictEqual(Calculator.compute( "0 / 1" ), 0)
-	test.ok(isNaN(Calculator.compute( "0 / 0" )))
-	test.strictEqual(Calculator.compute( "-25 / 25 / 0 / -0 / 9" ), Infinity)
-	test.strictEqual(Calculator.compute( "51 / 111 / 5 / 1000 / 2" ), 0.00004594594594594595)
-	test.done()
-}
+	it("substraction", () => {
+		assert.strictEqual(Calculator.compute( "0 - 1" ), -1)
+		assert.strictEqual(Calculator.compute( "1 - 1 - 13243" ), -13243)
+		assert.strictEqual(Calculator.compute( "51 - 111 - 5 - 1000 - 2" ), -1067)
+		assert.strictEqual(Calculator.compute( "234 - 324 - 23543.5 - 1.5" ), -23635)
+		assert.strictEqual(Calculator.compute( "-25 - 25 - 0 - -120 - 9" ), 61)
+	})
 
-export function multiplication(test) {
-	test.expect(5)
-	test.strictEqual(Calculator.compute( "0 * 1" ), 0)
-	test.strictEqual(Calculator.compute( "2 * + 150" ), 300)
-	test.strictEqual(Calculator.compute( "5 * - 180" ), -900)
-	test.strictEqual(Calculator.compute( "51 * 111 * 5 * 1000 * 2" ), 56610000)
-	test.strictEqual(Calculator.compute( "3 * 111.5" ), 334.5)
-	test.done()
-}
+	it("division", () => {
+		assert.strictEqual(Calculator.compute( "0 / 1" ), 0)
+		assert.ok(isNaN(Calculator.compute( "0 / 0" )))
+		assert.strictEqual(Calculator.compute( "-25 / 25 / 0 / -0 / 9" ), Infinity)
+		assert.strictEqual(Calculator.compute( "51 / 111 / 5 / 1000 / 2" ), 0.00004594594594594595)
+	})
 
-export function modulo(test) {
-	test.expect(7)
-	test.strictEqual(Calculator.compute( "0 % 1" ), 0)
-	test.strictEqual(Calculator.compute( "0 % + 121" ), 0)
-	test.strictEqual(Calculator.compute( "0 % - 990" ), 0)
-	test.strictEqual(Calculator.compute( "23 % 12 % 511" ), 11)
-	test.strictEqual(Calculator.compute( "5 % 1555 % 555" ), 5)
-	test.ok(isNaN(Calculator.compute( "1 % 0" )))
-	test.strictEqual(Calculator.compute( "3 % 111.5" ), 3)
-	test.done()
-}
+	it("multiplication", () => {
+		assert.strictEqual(Calculator.compute( "0 * 1" ), 0)
+		assert.strictEqual(Calculator.compute( "2 * + 150" ), 300)
+		assert.strictEqual(Calculator.compute( "5 * - 180" ), -900)
+		assert.strictEqual(Calculator.compute( "51 * 111 * 5 * 1000 * 2" ), 56610000)
+		assert.strictEqual(Calculator.compute( "3 * 111.5" ), 334.5)
+	})
 
-export function order(test) {
-	test.expect(3)
-	test.strictEqual(Calculator.compute( "5 + 5 * 2" ), 15)
-	test.strictEqual(Calculator.compute( "5 - 1 / 2" ), 4.5)
-	test.strictEqual(Calculator.compute( "990 - 7 % 10" ), 983)
-	test.done()
-}
+	it("modulo", () => {
+		assert.strictEqual(Calculator.compute( "0 % 1" ), 0)
+		assert.strictEqual(Calculator.compute( "0 % + 121" ), 0)
+		assert.strictEqual(Calculator.compute( "0 % - 990" ), 0)
+		assert.strictEqual(Calculator.compute( "23 % 12 % 511" ), 11)
+		assert.strictEqual(Calculator.compute( "5 % 1555 % 555" ), 5)
+		assert.ok(isNaN(Calculator.compute( "1 % 0" )))
+		assert.strictEqual(Calculator.compute( "3 % 111.5" ), 3)
+	})
 
-export function parentheses(test) {
-	test.expect(3)
-	test.strictEqual(Calculator.compute( "38 +- 14 + (91 + 1) + 3 +- 2" ), 117)
-	test.strictEqual(Calculator.compute( "234 - (324 - 23543.5) - 1.5" ), 23452)
-	test.strictEqual(Calculator.compute( "51 / 111 / (5 / 1000) / 2" ), 45.94594594594595)
-	test.done()
-}
+	it("order", () => {
+		assert.strictEqual(Calculator.compute( "5 + 5 * 2" ), 20)
+		assert.strictEqual(Calculator.compute( "5 - 1 / 2" ), 4.5)
+		assert.strictEqual(Calculator.compute( "990 - 7 % 10" ), 983)
+	})
 
-export function multipleParentheses(test) {
-	test.expect(3)
-	test.strictEqual(Calculator.compute( "122 * (54534 * 5) * 1 * (2 * 4)" ), 266125920)
-	test.strictEqual(Calculator.compute( "122 * (525.34 / 5) % 1 * (2 + 4)" ), 1.7760000000125729)
-	test.strictEqual(Calculator.compute( "122 * (54534 * 5) / 6.5 * (2 - 4)" ), -10235612.307692308)
-	test.done()
-}
+	it("parentheses", () => {
+		assert.strictEqual(Calculator.compute( "38 +- 14 + (91 + 1) + 3 +- 2" ), 117)
+		assert.strictEqual(Calculator.compute( "234 - (324 - 23543.5) - 1.5" ), 23452)
+		assert.strictEqual(Calculator.compute( "51 / 111 / (5 / 1000) / 2" ), 45.94594594594595)
+	})
 
-export function nestedParentheses(test) {
-	test.expect(7)
-	test.strictEqual(Calculator.compute( "51 * 111 * (5 * 1000 * (5 * 5)) * 2" ), 1415250000)
-	test.strictEqual(Calculator.compute( "51 * 111 * (5 * -1000 * (5 * 5)) * 2" ), -1415250000)
-	test.strictEqual(Calculator.compute( "51 * 111 * (5 * 1000.231243 * (5 * 5))" ), 707788633.327875)
-	test.strictEqual(Calculator.compute( "5 + (5 * (2 * 2.432423 / (5 * 2)))" ),  7.432423)
-	test.strictEqual(Calculator.compute( "(2 * (5 + (5 * (2 * 2.432423 / (5 * 2)))))" ), 14.864846)
-	test.strictEqual(Calculator.compute( "51 * 111 * (5 * 1000.231243 * (5 * 5)) * (2 * (5 + (5 * (2 * 2.432423 / (5 * 2)))))" ), 10521169034.96933)
-	test.strictEqual(Calculator.compute( "511 % (111 % 5)" ), 0)
-	test.done()
-}
+	it("multipleParentheses", () => {
+		assert.strictEqual(Calculator.compute( "122 * (54534 * 5) * 1 * (2 * 4)" ), 266125920)
+		assert.strictEqual(Calculator.compute( "122 * (525.34 / 5) % 1 * (2 + 4)" ), 1.7760000000125729)
+		assert.strictEqual(Calculator.compute( "122 * (54534 * 5) / 6.5 * (2 - 4)" ), -10235612.307692308)
+	})
+
+	it("nestedParentheses", () => {
+		assert.strictEqual(Calculator.compute( "51 * 111 * (5 * 1000 * (5 * 5)) * 2" ), 1415250000)
+		assert.strictEqual(Calculator.compute( "51 * 111 * (5 * -1000 * (5 * 5)) * 2" ), -1415250000)
+		assert.strictEqual(Calculator.compute( "51 * 111 * (5 * 1000.231243 * (5 * 5))" ), 707788633.327875)
+		assert.strictEqual(Calculator.compute( "5 + (5 * (2 * 2.432423 / (5 * 2)))" ),  7.432423)
+		assert.strictEqual(Calculator.compute( "(2 * (5 + (5 * (2 * 2.432423 / (5 * 2)))))" ), 14.864846)
+		assert.strictEqual(Calculator.compute( "51 * 111 * (5 * 1000.231243 * (5 * 5)) * (2 * (5 + (5 * (2 * 2.432423 / (5 * 2)))))" ), 10521169034.96933)
+		assert.strictEqual(Calculator.compute( "511 % (111 % 5)" ), 0)
+	})
+
+})
--- test/parser.test.js
+++ test/parser.test.js
@@ -1,162 +1,155 @@
+import assert from "assert"
 import { Parser } from '../index.js'
 
-// export function invalid(test) {
-// 	test.expect(11)
-// 	test.throws(() =>	Parser.parse("38 +* 14"), Error)
-// 	// test.throws(() =>	Parser.parse("38 ++ 14"), Error) // throws if there is no space in between the plus signs
-// 	test.throws(() =>	Parser.parse("* 38"), Error)
-// 	test.throws(() =>	Parser.parse("-25 - -120 -"), Error)
-// 	test.throws(() =>	Parser.parse("-25 - -120 - -"), Error)
-// 	test.throws(() =>	Parser.parse("-25 - --120"), Error)
-// 	test.throws(() =>	Parser.parse("32 + 32 - 32."), Error)
-// 	test.throws(() =>	Parser.parse("32 + .32"), Error)
-// 	test.throws(() => Parser.parse("5 * (5 * - 180"), Error, "All groups should be closed")
-// 	test.doesNotThrow(() =>	Parser.parse("32 + 32.5"), Error, "Decimal separator should not throw when used before and after a digit.")
-// 	test.doesNotThrow(() => Parser.parse("2 * + 150"), Error, "We should be able to multiply an operand prefixed with a plus sign")
-// 	test.doesNotThrow(() => Parser.parse("5 * - 180"), Error, "We should be able to multiply a negative operand")
-// 	// test.throws(() =>	Parser.parse("61d2 - 5"), Error) // SyntaxError: identifier starts immediately after numeric literal
-// 	// test.throws(() =>	Parser.parse("3 2 + 5"), Error) // SyntaxError: unexpected token: numeric literal
-// 	test.done()
-// }
+describe("parser", () => {
 
-export function schema(test) {
-	test.expect(1)
-	const tokens = Parser.parse("3 * (8.5 + 9) * (23 + 5) * 2")
-	test.deepEqual(tokens, [
-		{ name: 'operand', buffer: 3 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 17.5 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 28 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 2 }
-	])
-	test.done()
-}
+	it("invalid", () => {
+		assert.throws(() =>	Parser.parse("38 +* 14"), Error)
+		// assert.throws(() =>	Parser.parse("38 ++ 14"), Error) // throws if there is no space in between the plus signs
+		assert.throws(() =>	Parser.parse("* 38"), Error)
+		assert.throws(() =>	Parser.parse("-25 - -120 -"), Error)
+		assert.throws(() =>	Parser.parse("-25 - -120 - -"), Error)
+		assert.throws(() =>	Parser.parse("-25 - --120"), Error)
+		assert.throws(() =>	Parser.parse("32 + 32 - 32."), Error)
+		assert.throws(() =>	Parser.parse("32 + .32"), Error)
+		assert.throws(() => Parser.parse("5 * (5 * - 180"), Error, "All groups should be closed")
+		assert.doesNotThrow(() =>	Parser.parse("32 + 32.5"), Error, "Decimal separator should not throw when used before and after a digit.")
+		assert.doesNotThrow(() => Parser.parse("2 * + 150"), Error, "We should be able to multiply an operand prefixed with a plus sign")
+		assert.doesNotThrow(() => Parser.parse("5 * - 180"), Error, "We should be able to multiply a negative operand")
+		// assert.throws(() =>	Parser.parse("61d2 - 5"), Error) // SyntaxError: identifier starts immediately after numeric literal
+		// assert.throws(() =>	Parser.parse("3 2 + 5"), Error) // SyntaxError: unexpected token: numeric literal
+	})
 
-export function buffer(test) {
-	test.expect(7)
+	it("schema", () => {
+		const tokens = Parser.parse("3 * (8.5 + 9) * (23 + 5) * 2")
+		assert.deepEqual(tokens, [
+			{ name: 'operand', buffer: 3 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 17.5 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 28 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 2 }
+		])
+	})
 
-	test.strictEqual(Parser.parse("4345345")[0].buffer, 4345345)
-	test.strictEqual(Parser.parse("3 + 1")[2].buffer, 1)
-	test.strictEqual(Parser.parse("25 + (6)")[1].buffer, "+")
-	test.strictEqual(Parser.parse("25 + (6)")[2].buffer, 6)
-	test.strictEqual(Parser.parse("98 + 20 * (45 * 2)")[0].buffer, 98)
-	test.strictEqual(Parser.parse("98 + 20 * (45 * 2)")[2].buffer, 20)
-	test.strictEqual(Parser.parse("98 + 20 * (45 * 2)")[4].buffer, 90)
+	it("buffer", () => {
 
-	test.done()
-}
+		assert.strictEqual(Parser.parse("4345345")[0].buffer, 4345345)
+		assert.strictEqual(Parser.parse("3 + 1")[2].buffer, 1)
+		assert.strictEqual(Parser.parse("25 + (6)")[1].buffer, "+")
+		assert.strictEqual(Parser.parse("25 + (6)")[2].buffer, 6)
+		assert.strictEqual(Parser.parse("98 + 20 * (45 * 2)")[0].buffer, 98)
+		assert.strictEqual(Parser.parse("98 + 20 * (45 * 2)")[2].buffer, 20)
+		assert.strictEqual(Parser.parse("98 + 20 * (45 * 2)")[4].buffer, 90)
 
-export function parentheses(test) {
-	test.expect(3)
-	test.deepEqual(Parser.parse("38 +- 14 + (91 + 1) + 3 +- 2"), [
-		{ name: 'operand', buffer: 38 },
-		{ name: 'operator', buffer: '+' },
-		{ name: 'operand', buffer: -14 },
-		{ name: 'operator', buffer: '+' },
-		{ name: 'operand', buffer: 92 },
-		{ name: 'operator', buffer: '+' },
-		{ name: 'operand', buffer: 3 },
-		{ name: 'operator', buffer: '+' },
-		{ name: 'operand', buffer: -2 }
-	])
-	test.deepEqual(Parser.parse("234 - (324 - 23543.5) - 1.5"), [
-		{ name: 'operand', buffer: 234 },
-		{ name: 'operator', buffer: '-' },
-		{ name: 'operand', buffer: -23219.5 },
-		{ name: 'operator', buffer: '-' },
-		{ name: 'operand', buffer: 1.5 },
-	])
-	test.deepEqual(Parser.parse("51 / 111 / (5 / 1000) / 2"), [
-		{ name: 'operand', buffer: 51 },
-		{ name: 'operator', buffer: '/' },
-		{ name: 'operand', buffer: 111 },
-		{ name: 'operator', buffer: '/' },
-		{ name: 'operand', buffer:  0.005 },
-		{ name: 'operator', buffer: '/' },
-		{ name: 'operand', buffer:  2 },
-	])
-	test.done()
-}
+	})
 
-export function multipleParentheses(test) {
-	test.expect(3)
-	test.deepEqual(Parser.parse("122 * (54534 * 5) * 1 * (2 * 4)"), [
-		{ name: 'operand', buffer: 122 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 272670 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  1 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  8 },
-	])
-	test.deepEqual(Parser.parse("122 * (525.34 / 5) % 1 * (2 + 4)"), [
-		{ name: 'operand', buffer: 122 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  105.06800000000001 },
-		{ name: 'operator', buffer: '%' },
-		{ name: 'operand', buffer:  1 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  6 },
-	])
-	test.deepEqual(Parser.parse("122 * (54534 * 5) / 6.5 * (2 - 4)"), [
-		{ name: 'operand', buffer: 122 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  272670 },
-		{ name: 'operator', buffer: '/' },
-		{ name: 'operand', buffer:  6.5 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  -2 },
-	])
-	test.done()
-}
+	it("parentheses", () => {
+		assert.deepEqual(Parser.parse("38 +- 14 + (91 + 1) + 3 +- 2"), [
+			{ name: 'operand', buffer: 38 },
+			{ name: 'operator', buffer: '+' },
+			{ name: 'operand', buffer: -14 },
+			{ name: 'operator', buffer: '+' },
+			{ name: 'operand', buffer: 92 },
+			{ name: 'operator', buffer: '+' },
+			{ name: 'operand', buffer: 3 },
+			{ name: 'operator', buffer: '+' },
+			{ name: 'operand', buffer: -2 }
+		])
+		assert.deepEqual(Parser.parse("234 - (324 - 23543.5) - 1.5"), [
+			{ name: 'operand', buffer: 234 },
+			{ name: 'operator', buffer: '-' },
+			{ name: 'operand', buffer: -23219.5 },
+			{ name: 'operator', buffer: '-' },
+			{ name: 'operand', buffer: 1.5 },
+		])
+		assert.deepEqual(Parser.parse("51 / 111 / (5 / 1000) / 2"), [
+			{ name: 'operand', buffer: 51 },
+			{ name: 'operator', buffer: '/' },
+			{ name: 'operand', buffer: 111 },
+			{ name: 'operator', buffer: '/' },
+			{ name: 'operand', buffer:  0.005 },
+			{ name: 'operator', buffer: '/' },
+			{ name: 'operand', buffer:  2 },
+		])
+	})
 
-export function nestedParentheses(test) {
-	test.expect(4)
-	test.deepEqual(Parser.parse("51 * 111 * (5 * 1000 * (5 * 5)) * 2"), [
-		{ name: 'operand', buffer: 51 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  111 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  5000 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  25 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer:  2 },
-	])
-	test.deepEqual(Parser.parse("51 * 111 * (5 * -1000 * (5 * 5)) * 2"), [
-		{ name: 'operand', buffer: 51 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 111 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: -5000 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 25 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 2 },
-	])
-	test.deepEqual(Parser.parse("51 * 111 * (5 * 1000.231243 * (5 * 5)) * (2 * (5 + 5 * (2 * 2.432423 / (5 * 2))))"), [
-		{ name: 'operand', buffer: 51 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 111 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 5001.156215 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 25 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 2 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 10 },
-		{ name: 'operator', buffer: '*' },
-		{ name: 'operand', buffer: 4.864846 },
-		{ name: 'operator', buffer: '/' },
-		{ name: 'operand', buffer: 10 },
-	])
-	test.deepEqual(Parser.parse("511 % (111 % 5)"), [
-		{ name: 'operand', buffer: 511 },
-		{ name: 'operator', buffer: '%' },
-		{ name: 'operand', buffer:  1 },
-	])
-	test.done()
-}
+	it("multipleParentheses", () => {
+		assert.deepEqual(Parser.parse("122 * (54534 * 5) * 1 * (2 * 4)"), [
+			{ name: 'operand', buffer: 122 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 272670 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  1 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  8 },
+		])
+		assert.deepEqual(Parser.parse("122 * (525.34 / 5) % 1 * (2 + 4)"), [
+			{ name: 'operand', buffer: 122 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  105.06800000000001 },
+			{ name: 'operator', buffer: '%' },
+			{ name: 'operand', buffer:  1 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  6 },
+		])
+		assert.deepEqual(Parser.parse("122 * (54534 * 5) / 6.5 * (2 - 4)"), [
+			{ name: 'operand', buffer: 122 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  272670 },
+			{ name: 'operator', buffer: '/' },
+			{ name: 'operand', buffer:  6.5 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  -2 },
+		])
+	})
+
+	it("nestedParentheses", () => {
+		assert.deepEqual(Parser.parse("51 * 111 * (5 * 1000 * (5 * 5)) * 2"), [
+			{ name: 'operand', buffer: 51 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  111 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  5000 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  25 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer:  2 },
+		])
+		assert.deepEqual(Parser.parse("51 * 111 * (5 * -1000 * (5 * 5)) * 2"), [
+			{ name: 'operand', buffer: 51 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 111 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: -5000 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 25 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 2 },
+		])
+		assert.deepEqual(Parser.parse("51 * 111 * (5 * 1000.231243 * (5 * 5)) * (2 * (5 + 5 * (2 * 2.432423 / (5 * 2))))"), [
+			{ name: 'operand', buffer: 51 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 111 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 5001.156215 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 25 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 2 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 10 },
+			{ name: 'operator', buffer: '*' },
+			{ name: 'operand', buffer: 4.864846 },
+			{ name: 'operator', buffer: '/' },
+			{ name: 'operand', buffer: 10 },
+		])
+		assert.deepEqual(Parser.parse("511 % (111 % 5)"), [
+			{ name: 'operand', buffer: 511 },
+			{ name: 'operator', buffer: '%' },
+			{ name: 'operand', buffer:  1 },
+		])
+	})
+
+})
